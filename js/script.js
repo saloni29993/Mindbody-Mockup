8355ab20a6d11e9f3dad05eda854ecd7b0e83790
Saloni Shah <saloni29993@gmail.com>
2017-02-25 14:57:35 -0800

    improvements module css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-$('.nav.navbar-nav > li').on('click', function (e) {
-    e.preventDefault();
-    $('.nav.navbar-nav > li').removeClass('active');
-    $(this).addClass('active');
-});
-
 $(document).ready(function () {
     $(".module-extended").hide();
     $(".module-more").on("click", function () {
@@ -15,4 +9,29 @@ $(document).ready(function () {
     $(".module-new").click(function(){
     $("#newprogmodal").modal('show');
 	});
+
+	$('#sub-navigation ul').hide();
+
+	$('#set1').show();
+
+
+});
+
+// $('.nav.navbar-nav > li').on('click', function (e) {
+//     e.preventDefault();
+//     $('.nav.navbar-nav > li').removeClass('active');
+//     $(this).addClass('active');
+// });
+
+
+$('#navigation a').on('click', function(e){
+	e.preventDefault();
+
+	var submenu_id = $(this).attr('href');
+
+	//console.log(submenu_id);
+
+	$('#sub-navigation ul').hide();
+
+	$(submenu_id).show();
 });
