dbd7507015267c34fe843eab9b67d1e00fe17d89
Saloni Shah <saloni29993@gmail.com>
2017-02-24 17:23:46 -0800

    Basic modal form added

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ $('.nav.navbar-nav > li').on('click', function (e) {
     $('.nav.navbar-nav > li').removeClass('active');
     $(this).addClass('active');
 });
+
+$(document).ready(function () {
+    $(".module-extended").hide();
+    $(".module-more").on("click", function () {
+        var txt = $(".module-extended").is(':visible') ? 'More..' : 'Less..';
+        $(".module-more").text(txt);
+        $(this).next('.module-extended').slideToggle(200);
+    });
+});
+
+$(document).ready(function(){
+$(".module-new").click(function(){
+    $("#newprogmodal").modal('show');
+});
+});
